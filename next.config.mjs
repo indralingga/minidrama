f66c7feb4e5ebb392a7fdfa4c93ec9416a8aba89
unknown <devnewpipe@gmail.com>
2026-07-22 12:00:02 +0700

    Disable_build_linting_and_type_checks_in_production

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  typescript: {
+    // Allow production builds to complete even if there are TypeScript errors
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // Allow production builds to complete even if there are ESLint warning/errors
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
